5f37ad1e2f8daf8553239305af2fe477c2a2f704
Kevin <kevinjervis17@gmail.com>
2023-02-26 21:21:01 -0800

    Up to Docker

--- manage.py
+++ manage.py
@@ -1,8 +1,16 @@
 from flask.cli import FlaskGroup
-from src import app
+from src import app, db
+
 
 #  lets us use Flask command line arguments with our app
 cli = FlaskGroup(app)
+
+# add command to CLI
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 if __name__ == '__main__':
     cli()
--- src/config.py
+++ src/config.py
@@ -1,16 +1,20 @@
 # setting up class variables in specific environment
 
+import os
+
 class BaseConfig:
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  
 
 
 class DevelopmentConfig(BaseConfig):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  
 
 
 class TestingConfig(BaseConfig):
     TESTING = True
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL') 
+     
 
 class ProductionConfig(BaseConfig):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  
