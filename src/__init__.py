e3ba6f367753a3679b47e865685a71889324f767
Kevin <kevinjervis17@gmail.com>
2023-02-27 11:47:20 -0800

    SQL

--- src/__init__.py
+++ src/__init__.py
@@ -1,23 +1,19 @@
+import os
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
-from flask_sqlalchemy import SQLAlchemy
-import os
+from flask_sqlalchemy import SQLAlchemy  
 
 
-# initialize app
+# instantiate the app
 app = Flask(__name__)
 
-# initialize api and add environmental settings
 api = Api(app)
 
-
-app_settings = os.getenv('APP_SETTINGS')  
+# set config
+app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
 
-# pull in development environment config
-app.config.from_object('src.config.DevelopmentConfig')
-
-# initialize db
+# instantiate the db
 db = SQLAlchemy(app)  
 
 
@@ -38,7 +34,7 @@ class Ping(Resource):
     def get(self):
         return {
             'status': 'success',
-            'message': 'hello'
+            'message': 'pong!'
         }
 
 
